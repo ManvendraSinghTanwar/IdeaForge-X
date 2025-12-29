07064526b4e737997d7fcfb2d2c3709084ca763d
unknown <manvendrasinghtanwar34@gmail.com>
2025-12-29 22:43:15 +0530

    chore: upgrade Next.js to 16.1.1 to fix security vulnerability

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,22 +1,22 @@
 globalThis.__BUILD_MANIFEST = {
-  "polyfillFiles": [
-    "static/chunks/polyfills.js"
-  ],
-  "devFiles": [],
-  "ampDevFiles": [],
-  "lowPriorityFiles": [],
-  "rootMainFiles": [
-    "static/chunks/webpack.js",
-    "static/chunks/main-app.js"
-  ],
-  "rootMainFilesTree": {},
   "pages": {
     "/_app": []
   },
-  "ampFirstPages": []
+  "devFiles": [],
+  "polyfillFiles": [
+    "static/chunks/a6dad97d9634a72d.js"
+  ],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/c6bd443df4ae7342.js",
+    "static/chunks/d8571c0793dba9c7.js",
+    "static/chunks/eab824301c024821.js",
+    "static/chunks/dd1e98054550e04f.js",
+    "static/chunks/3affdeaf6a76fe83.js",
+    "static/chunks/turbopack-1806172ecbdbd41c.js"
+  ]
 };
 globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
 "/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
-,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
-
+"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js"
 ];
